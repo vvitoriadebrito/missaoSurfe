19d487e5462f797d3674a2275083b33197ccf8be
silviosnjr <silviosnjr@gmail.com>
2024-08-05 13:03:59 -0300

    ajustes

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ function mostraPergunta(){
 
 function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
-        const botaoAlternativas = document.createElement(button);
+        const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
         botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas)
